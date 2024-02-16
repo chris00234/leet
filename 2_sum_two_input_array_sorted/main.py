49528c8df1efddb64f4b996471cd82c69649f9f1
Chris <wheogus98@gmail.com>
2024-02-15 20:11:18 -0800

    167

---
+++ 2_sum_two_input_array_sorted/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            print(i, j)
+            if numbers[i] + numbers[j] == target:
+                return (i + 1, j + 1)
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                i += 1
+                 
