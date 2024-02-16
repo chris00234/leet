140a1078ea9ed191eebf38a20c81c393b43152ea
Chris <wheogus98@gmail.com>
2024-02-15 22:41:53 -0800

    209

---
+++ minimum_size_of_subarray_sum/mian.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        total = 0
+        ans = float("inf")
+        n = len(nums)
+        for r in range(n):
+            total += nums[r]
+            while total >= target:
+                ans = min(r - l + 1, ans)
+                total -= nums[l]
+                l += 1
+        
+        return 0 if ans == float("inf") else ans
