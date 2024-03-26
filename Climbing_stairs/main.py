4bbdf0125eeb8874fb33564e6f99bdcab9c30745
Chris <wheogus98@gmail.com>
2024-03-26 10:00:25 -0700

    70

--- Climbing_stairs/main.py
+++ Climbing_stairs/main.py
@@ -1,13 +1,32 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = [-1 for i in range(n + 1)]
+        # memo = [0 for i in range(n + 1)]
+    
+        # memo[0] = 1
+        # memo[1] = 1
+        
 
-        for i in range(n + 1):
-            if i < 3:
-                memo[i] = i
-            else:
-                memo[i] = memo[i - 1] + memo[i - 2]
-        return memo[n]
+        # for i in range(2, n + 1):
+        #     memo[i] = memo[i - 1] + memo[i - 2]
+        
+        # return memo[n]
+
+        if n <= 2:
+            return n
+
+        memo = {}
+        def recur(n):
+            if n < 2:
+                return 1
+            
+            if n not in memo:
+                memo[n] = recur(n - 1) + recur(n - 2)
+            return memo[n]
+
+        return recur(n)
+
+
+
 
         
 
