fbc1b4cb12a06f286170bcae1d9f4e43a5ae552f
Chris <wheogus98@gmail.com>
2024-03-31 17:02:14 -0700

    58

--- length_of_last_word/main.py
+++ length_of_last_word/main.py
@@ -1,16 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        st = ""
-        li = []
-        for char in s:
-            if char != ' ':
-                st += char
-            else:
-                if st != "":
-                    li.append(st)
-                st = ""
-        if st != "":
-            li.append(st)
-        # print(li)
-        return len(li[-1])
+       li = s.split()
+       return len(li[-1])
         
