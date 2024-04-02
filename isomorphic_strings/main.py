264b9265b1fdccca7b8e38833b0df481fb3ea523
Chris <wheogus98@gmail.com>
2024-04-01 17:10:52 -0700

    205

--- isomorphic_strings/main.py
+++ isomorphic_strings/main.py
@@ -1,17 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        
+        n = len(s)
+        mp = {}
+        if len(set(s)) != len(set(t)):
             return False
         
-        dict = {x: None for x in s}
-        for i in range(len(s)):
-            if dict[s[i]] == None:
-                if t[i] in dict.values():
+        for i in range(n):
+            if s[i] in mp:
+                if mp[s[i]] != t[i]:
                     return False
-                dict[s[i]] = t[i]
             else:
-                if dict[s[i]] != t[i]:
-                    return False
+                mp[s[i]] = t[i]
 
+        
         return True
         
