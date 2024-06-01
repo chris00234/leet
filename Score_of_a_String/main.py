1fa9ab0fd8bec0e8fa36fe4d4d5031602853172a
chris cho <wheogus98@gmail.com>
2024-05-31 17:40:40 -0700

    3110

---
+++ Score_of_a_String/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(1, len(s)):
+            score += abs(ord(s[i - 1]) - ord(s[i]))
+        return score
