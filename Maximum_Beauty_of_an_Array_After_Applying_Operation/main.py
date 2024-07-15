f14710b700cc4deff1384c035384a4aeb68a096c
Chris <wheogus98@gmail.com>
2024-07-15 12:49:47 -0700

    2779

---
+++ Maximum_Beauty_of_an_Array_After_Applying_Operation/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l = 0
+        ans = 0
+
+        for r in range(len(nums)):
+            while nums[r] - nums[l] > 2 * k:
+                l += 1
+            ans = max(ans, r - l + 1)
+        return ans
+        
+        
+            
